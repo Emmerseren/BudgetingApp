b59e5ef4ab4b622f32ea7a106a6c62083203f8d7
Emmerseren <fyrstenberg42@gmail.com>
2023-04-07 10:22:32 +0200

    fourth  deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-minimal-pie-chart"],
+      external: ["react", "react-dom", "react-minimal-pie-chart"],
     },
   },
 });
